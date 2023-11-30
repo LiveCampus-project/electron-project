925747f6f610d3c1a907dc4b37ab7ca658d129be
Moussa Bamba <moussabulls1@gmail.com>
2023-11-30 14:04:12 +0100

    interfaces

---
+++ src/interfaces/event.ts
@@ -0,0 +1,11 @@
+export interface IEvent {
+    id?: number,
+    date_debut: string,
+    date_fin: string,
+    titre: string,
+    location: string,
+    status: string,
+    description: string,
+    transparence: string,
+}
+
